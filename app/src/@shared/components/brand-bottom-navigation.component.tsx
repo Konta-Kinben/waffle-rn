67c0187a84efa9064eb34fa63fc8ade0aeeb9655
Jobayer Hossain <jobayerhossain977@gmail.com>
2021-09-18 23:40:32 +0600

    master: Added

--- app/src/@shared/components/brand-bottom-navigation.component.tsx
+++
@@ -1,9 +0,0 @@
-import { BottomNavigation, BottomNavigationProps } from "@ui-kitten/components"
-
-import React from "react"
-
-export const BrandBottomNavigation = (
-	props: BottomNavigationProps
-): React.ReactElement => {
-	return <BottomNavigation {...props} />
-}
--- app/src/ENV.ts
+++
@@ -1,4 +0,0 @@
-export const ENV = {
-  CORE_END_POINT: "https://mighty-mickey.poshapets.com/api/v1/",
-  SESSION_PREFIX: "PPL-",
-};
