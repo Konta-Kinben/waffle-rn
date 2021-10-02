c265565404ffab8870166d0fb7c519dc349e7b5c
Jobayer Hossain <jobayerhossain977@gmail.com>
2021-10-02 21:21:58 +0600

    ini

--- waffle/src/@application/assets/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as MUSIC_FLY_LOTTIE } from "../assets/lottie-animations/music-fly.json";
--- waffle/src/@application/config/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./axios/core-axios-instantance"
-export * from "./react-query/react-query"
